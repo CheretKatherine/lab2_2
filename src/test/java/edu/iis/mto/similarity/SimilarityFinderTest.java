57167238cf74ca11e2ee086a80cd766a0da40020
Katherine <ocheretianfws@gmail.com>
2021-03-22 20:09:28 +0200

    changed test()

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -2,13 +2,26 @@ package edu.iis.mto.similarity;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderTest {
 
     @Test
     void test() {
-        fail("Not yet implemented");
+        SequenceSearcher ss = new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                for(int i=0; i< sequence.length; i++){
+                    if(sequence[i]==elem){
+                        return SearchResult.Builder.withFound(true).withPosition(i).build();;
+                    }
+                }
+                return SearchResult.Builder.withFound(false).build();;
+            }
+        }
+
     }
 
 }
